6a134e43720dff683b70e7272c1de0d03d5d6d91
Vu Thanh Tung <vuthanhtung1891995@gmail.com>
2023-03-28 01:34:22 +0700

    sso

--- src/main/java/com/lg/controller/UserController.java
+++ src/main/java/com/lg/controller/UserController.java
@@ -1,67 +1,67 @@
-package com.lg.controller;
-
-import com.lg.model.User;
-import com.lg.service.SecurityService;
-import com.lg.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class UserController {
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private SecurityService securityService;
-
-    @Autowired
-    private UserValidator userValidator;
-
-    @GetMapping("/registration")
-    public String registration(Model model) {
-        if (securityService.isAuthenticated()) {
-            return "redirect:/";
-        }
-
-        model.addAttribute("userForm", new User());
-
-        return "registration";
-    }
-
-    @PostMapping("/registration")
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
-        userValidator.validate(userForm, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "registration";
-        }
-
-        userService.save(userForm);
-
-        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
-
-        return "redirect:/issues";
-    }
-
-    @GetMapping("/login")
-    public String login(Model model, String error, String logout) {
-        if (securityService.isAuthenticated()) {
-            return "redirect:/";
-        }
-
-        if (error != null)
-            model.addAttribute("error", "Your username or password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
-        return "login";
-    }
-
-
-}
+//package com.lg.controller;
+//
+//import com.lg.model.User;
+//import com.lg.service.SecurityService;
+//import com.lg.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class UserController {
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private SecurityService securityService;
+//
+//    @Autowired
+//    private UserValidator userValidator;
+//
+//    @GetMapping("/registration")
+//    public String registration(Model model) {
+//        if (securityService.isAuthenticated()) {
+//            return "redirect:/";
+//        }
+//
+//        model.addAttribute("userForm", new User());
+//
+//        return "registration";
+//    }
+//
+//    @PostMapping("/registration")
+//    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+//        userValidator.validate(userForm, bindingResult);
+//
+//        if (bindingResult.hasErrors()) {
+//            return "registration";
+//        }
+//
+//        userService.save(userForm);
+//
+//        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+//
+//        return "redirect:/issues";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login(Model model, String error, String logout) {
+//        if (securityService.isAuthenticated()) {
+//            return "redirect:/";
+//        }
+//
+//        if (error != null)
+//            model.addAttribute("error", "Your username or password is invalid.");
+//
+//        if (logout != null)
+//            model.addAttribute("message", "You have been logged out successfully.");
+//
+//        return "login";
+//    }
+//
+//
+//}
--- src/main/java/com/lg/service/UserServiceImpl.java
+++ src/main/java/com/lg/service/UserServiceImpl.java
@@ -9,24 +9,30 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 
-@Service
-public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    public void save(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRoles(new HashSet<>(roleRepository.findAll()));
-        userRepository.save(user);
-    }
-
-    @Override
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-}
+//@Service
+//public class UserServiceImpl implements UserService {
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private RoleRepository roleRepository;
+//
+//    @Override
+//    public void save(User user) {
+//
+//    }
+//
+//    @Autowired
+////    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+////    @Override
+////    public void save(User user) {
+////        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+////        user.setRoles(new HashSet<>(roleRepository.findAll()));
+////        userRepository.save(user);
+////    }
+//
+//    @Override
+//    public User findByUsername(String username) {
+//        return userRepository.findByUsername(username);
+//    }
+//}
